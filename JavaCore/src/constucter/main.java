02f38c19ed5b4203bc48f2ad9989646138301456
boatmello2601 <pawee.boat@gmail.com>
2025-04-06 18:19:45 +0700

    Add new Object
    Add new Object

---
+++ JavaCore/src/constucter/main.java
@@ -0,0 +1,9 @@
+package constucter;
+
+public class main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        Student student1 = new Student(1, "oat");
+    }
+}
